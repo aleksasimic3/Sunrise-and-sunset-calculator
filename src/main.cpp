1b1281e5ffc52b4d19b1a11dadf84f1c2856712c
Aleksa Simić <aleksasimic8@gmail.com>
2025-02-04 01:32:49 +0100

    Added GPS and time zone information output.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 #include "sunrise.h"
 
@@ -10,11 +11,20 @@ int main() {
     const double elevation = 117;
     const int timezone = 1;
 
+    std::cout << '\n';
+    std::cout << (latitude>=0?'N':'S') << std::abs(latitude) << ' ' << (longitude>=0?'E':'W') << std::abs(longitude) << '\n';
+    std::cout << elevation << "m elevation\n";
+    std::cout << "GMT" << (timezone>=0?'+':'-') << timezone << '\n';
+
+    time_t currentTime = std::time(NULL);
+    std::cout << '\n' <<  std::ctime(&currentTime) << '\n';
+
+    //izgleda da ctime pazi o vremenskim zonama :)
     time_t sunrise = getSunrise(latitude, longitude, elevation, timezone);
-    std::cout << "\nSunrise: " << std::ctime(&sunrise) << '\n';
+    std::cout << "Sunrise: " << std::ctime(&sunrise);
 
     time_t sunset = getSunset(latitude, longitude, elevation, timezone);
-    std::cout << "Sunset: " << std::ctime(&sunset) << '\n';
+    std::cout << "Sunset: " << std::ctime(&sunset);
 
     return 0;
 }
